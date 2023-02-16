1de2080a2884b63d541e338f5705c34e4684460a
瀬林知哉 <sebayashitomoya@MacBook.local>
2023-02-16 12:01:08 +0900

    Lesson_easylogin

---
+++ easylogin/src/test/java/com/example/eassylogin/EasyloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eassylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
